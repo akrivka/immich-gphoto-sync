52a239753edc3f8098e7d99fbc43aae0aa87b113
Adam Krivka <krivka.adam@gmail.com>
2025-03-09 12:08:13 +0100

    add dotenv

--- config.py
+++ config.py
@@ -1,5 +1,16 @@
 import os
+import dotenv
 from pathlib import Path
+
+# Try to load from data dir if specified in environment
+data_folder = os.environ.get("DATA_FOLDER")
+if data_folder:
+    env_path = Path(data_folder) / ".env"
+    if env_path.exists():
+        dotenv.load_dotenv(dotenv_path=env_path)
+else:
+    # Fall back to current directory
+    dotenv.load_dotenv()
 
 IMMICH_URL = os.environ["IMMICH_URL"]
 IMMICH_API_KEY = os.environ["IMMICH_API_KEY"]
